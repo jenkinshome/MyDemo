61f7674219773b7701016f612b0ad844615a4839
jenkinshome <jenkinsgithub409@gmail.com>
2022-05-25 14:16:15 +0530

    ignore message

--- MyDemo.java
+++ MyDemo.java
@@ -2,6 +2,8 @@ class MyDemo {
  public static void main(String args[]) {
   System.out.println("Demo Test...");
   System.out.println("......");
+  System.out.println("..");
+  System.out.println("......");
  }
 }
  
